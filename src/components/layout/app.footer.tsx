e8773bf808ba1a4aec9a2edf9c44be4c75cee4db
TienAn <tienan05@gmail.com>
2026-01-09 10:12:00 +0700

    Change skils

--- src/components/layout/app.footer.tsx
+++ src/components/layout/app.footer.tsx
@@ -1,12 +1,21 @@
+import { useTranslation } from 'react-i18next';
 
 const AppFooter = () => {
+    const { t } = useTranslation();
     return (
-        <div className="text-center my-3">
-            Copyright © {new Date().getFullYear()} Hỏi Dân IT vs Eric. Made with <span style={{
-                color: "#e25555"
-            }}>♥</span> in VietNam
+        <div className='text-center my-3'>
+            Copyright © {new Date().getFullYear()} {t('appHeader.brand')}. Made
+            with{' '}
+            <span
+                style={{
+                    color: '#e25555',
+                }}
+            >
+                ♥
+            </span>{' '}
+            in VietNam
         </div>
-    )
-}
+    );
+};
 
 export default AppFooter;
